b5d993a9bf65ee669ea98fcaf6838cc4cc98abf0
xubuntus <nikita.dmd@gmail.com>
2021-10-20 01:34:57 +0300

    gui

--- SB-2.1.1/ViewController.swift
+++ SB-2.1.1/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redCircleView: UIView!
+    @IBOutlet var yellowCircleView: UIView!
+    @IBOutlet var greenCircleView: UIView!
+    @IBOutlet var startButton: UIButton!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        redCircleView.layer.cornerRadius = redCircleView.frame.width / 2
+        redCircleView.alpha = 0.3
+        yellowCircleView.layer.cornerRadius = yellowCircleView.frame.width / 2
+        yellowCircleView.alpha = 0.3
+        greenCircleView.layer.cornerRadius = greenCircleView.frame.width / 2
+        greenCircleView.alpha = 0.3
+        
+        startButton.layer.cornerRadius = 10
+        print(greenCircleView.frame.width, greenCircleView.frame.height)
     }
 
-
+    @IBAction func startAction(_ sender: Any) {
+    }
 }
 
